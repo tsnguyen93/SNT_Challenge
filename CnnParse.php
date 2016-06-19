7847005f0d6302a1a15135a2ed14c7b5477b9c24
Simon <tsnguyen@wichita.edu>
2016-06-19 16:06:03 -0500

    final commit

--- CnnParse.php
+++ CnnParse.php
@@ -1,15 +1,21 @@
 <?php
 class Article{
-	Protected $doc;
+	Protected $url;
 	
-	Public function __construct($doc){
-		$this->doc = $doc;
+	Public function __construct($url){
+		$this->url = $url;
 	}
 	
 	// This is grabs every content within an h1 tag.
 	// This captures the headline of the article.
 	Public function getHeadline(){
-		$headers = $this->doc->getElementsByTagName('h1');
+		$string = file_get_contents($this->url);
+		$doc = new DOMDocument();
+		libxml_use_internal_errors(true);
+		$doc->loadHTML($string);
+		libxml_use_internal_errors(false);
+		
+		$headers = $doc->getElementsByTagName('h1');
 		$headers_arr = array();
 		
 			foreach ($headers as $header){
@@ -29,7 +35,13 @@ class Article{
 	//There were multiple images of the same image but different size.
 	//This captures one of the images to display but iterates through all images scraped.
 	Public function getImage(){
-		$images = $this->doc->getElementsByTagName('img');
+		$string = file_get_contents($this->url);
+		$doc = new DOMDocument();
+		libxml_use_internal_errors(true);
+		$doc->loadHTML($string);
+		libxml_use_internal_errors(false);
+		
+		$images = $doc->getElementsByTagName('img');
 		$images_arr = array();
 		
 		foreach ($images as $image){
@@ -51,8 +63,15 @@ class Article{
 	//Part of the story was located in p tags and the rest was located in the div class = "zn-body__paragraph".
 	//This grabs everything and stores each value into an array and returns it to the user.
 	Public function getStory(){
-		$stories = $this->doc->getElementsByTagName('p');
-		$xpath = new DOMXPath($this->doc);		
+		$string = file_get_contents($this->url);
+		$doc = new DOMDocument();
+		libxml_use_internal_errors(true);
+		$doc->loadHTML($string);
+		libxml_use_internal_errors(false);
+		
+		$xpath = new DOMXPath($doc);	
+		
+		$stories = $doc->getElementsByTagName('p');		
 		$stories_arr = array ();
 		
 		foreach ($stories as $story){
@@ -81,16 +100,7 @@ if ($url != 'http://www.cnn.com/2016/06/16/design/nasa-mars-posters/index.html')
 }
 else{
 
-//Creating DOM document and Xpath objects to parse through
-	$string = file_get_contents($url);
-	$doc = new DOMDocument();
-	libxml_use_internal_errors(true);
-	$doc->loadHTML($string);
-	libxml_use_internal_errors(false);
-	$xpath = new DOMXPath($doc);
-
-
-	$article = new Article($doc);
+	$article = new Article($url);
 	
 	$headline = "<h1>" . $article->getHeadline() . "</h1>";
 	echo $headline;
