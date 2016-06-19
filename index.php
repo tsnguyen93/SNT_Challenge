9006d54970e86f1a93093ad834a22928f0bd5d38
Simon <tsnguyen@wichita.edu>
2016-06-19 03:28:47 -0500

    final commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <html>
 <body>
-
+<!-- Simple submission form-->
 <form action="CnnParse.php" method="GET">
 Use the URL <b>http://www.cnn.com/2016/06/16/design/nasa-mars-posters/index.html</b> <input type="text" name="article">
 <input type="submit" value="Scrape!">
