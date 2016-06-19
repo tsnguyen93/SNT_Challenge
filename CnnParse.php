e059228ecf78e0ff6bab93d90ab8059a3538a4d1
Simon <tsnguyen@wichita.edu>
2016-06-19 16:14:42 -0500

    final commit

--- CnnParse.php
+++ CnnParse.php
@@ -1,21 +1,32 @@
 <?php
-class Article{
+class DomDoc{
 	Protected $url;
 	
 	Public function __construct($url){
 		$this->url = $url;
 	}
 	
-	// This is grabs every content within an h1 tag.
-	// This captures the headline of the article.
-	Public function getHeadline(){
+	Public function GetDom(){
 		$string = file_get_contents($this->url);
 		$doc = new DOMDocument();
 		libxml_use_internal_errors(true);
 		$doc->loadHTML($string);
 		libxml_use_internal_errors(false);
+		return $doc;
+	}
+}
+class Article{
+	Protected $doc;
+	
+	Public function __construct($doc){
+		$this->doc = $doc;
+	}
+	
+	// This is grabs every content within an h1 tag.
+	// This captures the headline of the article.
+	Public function getHeadline(){
 		
-		$headers = $doc->getElementsByTagName('h1');
+		$headers = $this->doc->getElementsByTagName('h1');
 		$headers_arr = array();
 		
 			foreach ($headers as $header){
@@ -35,13 +46,8 @@ class Article{
 	//There were multiple images of the same image but different size.
 	//This captures one of the images to display but iterates through all images scraped.
 	Public function getImage(){
-		$string = file_get_contents($this->url);
-		$doc = new DOMDocument();
-		libxml_use_internal_errors(true);
-		$doc->loadHTML($string);
-		libxml_use_internal_errors(false);
 		
-		$images = $doc->getElementsByTagName('img');
+		$images = $this->doc->getElementsByTagName('img');
 		$images_arr = array();
 		
 		foreach ($images as $image){
@@ -63,15 +69,10 @@ class Article{
 	//Part of the story was located in p tags and the rest was located in the div class = "zn-body__paragraph".
 	//This grabs everything and stores each value into an array and returns it to the user.
 	Public function getStory(){
-		$string = file_get_contents($this->url);
-		$doc = new DOMDocument();
-		libxml_use_internal_errors(true);
-		$doc->loadHTML($string);
-		libxml_use_internal_errors(false);
 		
-		$xpath = new DOMXPath($doc);	
+		$xpath = new DOMXPath($this->doc);	
 		
-		$stories = $doc->getElementsByTagName('p');		
+		$stories = $this->doc->getElementsByTagName('p');		
 		$stories_arr = array ();
 		
 		foreach ($stories as $story){
@@ -99,8 +100,11 @@ if ($url != 'http://www.cnn.com/2016/06/16/design/nasa-mars-posters/index.html')
 	echo "Please go back and use the link " . "<b>" . "http://www.cnn.com/2016/06/16/design/nasa-mars-posters/index.html" . "</b>" ;
 }
 else{
-
-	$article = new Article($url);
+	
+	$doc = new DomDoc($url);
+	$doc = $doc->GetDom();
+	
+	$article = new Article($doc);
 	
 	$headline = "<h1>" . $article->getHeadline() . "</h1>";
 	echo $headline;
